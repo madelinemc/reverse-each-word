c08755c47d3dd7b7ab183584468c5d09ab9901e2
madelinemc <madeline.coons@gmail.com>
2020-10-11 12:44:04 -0500

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,14 @@
+#FIRST SOLVE USING EACH:
+# def reverse_each_word(string)
+#     split_string = string.split(" ")
+#     reversed = []
+#     split_string.each { |word| reversed << word.reverse }
+#     reversed.join(" ")
+# end
 
+#THEN SOLVE USING COLLECT
 def reverse_each_word(string)
     split_string = string.split(" ")
-    reversed = []
-    split_string.each { |word| reversed << word.reverse }
-    reversed.join(" ")
-end
-
-def reverse_each_word_2(string)
-    split_string = string.split(" ")
-    split_string.collect { |word| word.reverse}
-    split_string.join(" ")
+    new_string_split = split_string.collect { |word| word.reverse}
+    new_string_split.join(" ")
 end
